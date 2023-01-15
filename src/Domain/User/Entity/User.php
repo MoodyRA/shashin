57227f630ef9c0c9cbf93c26962b4771e00052bf
Ronan <ronan.aupetit@gmail.com>
2023-01-15 19:24:32 +0100

    création d'une entité User

---
+++ src/Domain/User/Entity/User.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shashin\Domain\User\Entity;
+
+use Shashin\Domain\User\Model\Credential;
+use Shashin\Shared\Entity\Entity;
+
+class User extends Entity
+{
+    public function __construct(
+        protected Credential $credential,
+        protected string $name,
+        protected bool $isAdmin = false,
+    ) {
+    }
+
+    /**
+     * @return Credential
+     */
+    public function getCredential(): Credential
+    {
+        return $this->credential;
+    }
+
+    /**
+     * @param Credential $credential
+     * @return User
+     */
+    public function setCredential(Credential $credential): User
+    {
+        $this->credential = $credential;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return User
+     */
+    public function setName(string $name): User
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->isAdmin;
+    }
+
+    /**
+     * @param bool $isAdmin
+     * @return User
+     */
+    public function setIsAdmin(bool $isAdmin): User
+    {
+        $this->isAdmin = $isAdmin;
+        return $this;
+    }
+}
---
+++ src/Domain/User/Model/Credential.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Shashin\Domain\User\Model;
+
+use Moody\ValueObject\Auth\HashedPassword;
+use Moody\ValueObject\Web\EmailAddress;
+
+class Credential
+{
+    public function __construct(
+        protected EmailAddress $email,
+        protected HashedPassword $password
+    ) {
+    }
+
+    /**
+     * @return EmailAddress
+     */
+    public function getEmail(): EmailAddress
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param EmailAddress $email
+     * @return Credential
+     */
+    public function setEmail(EmailAddress $email): Credential
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return HashedPassword
+     */
+    public function getPassword(): HashedPassword
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param HashedPassword $password
+     * @return Credential
+     */
+    public function setPassword(HashedPassword $password): Credential
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
